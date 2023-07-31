e63daf2f1e8f896d6c19df311de6dd3b72e8149e
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2023-07-31 14:33:19 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    var answer = '';
+    if(num%2==0){
+        answer = 'Even';
+    }else if(num%2==1||num%2==-1){
+        answer = 'Odd';
+    }
+    return answer;
+}
