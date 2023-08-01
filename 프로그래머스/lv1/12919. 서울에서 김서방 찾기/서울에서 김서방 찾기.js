81dafa6e101088f539347ba1275aacc539492768
suhyunlee <118110277+suhyunlee01@users.noreply.github.com>
2023-08-01 17:13:09 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,7 @@
+function solution(seoul) {
+    for(var i = 0; i<seoul.length; i++){
+        if(seoul[i] == "Kim"){
+            var answer = "김서방은 " + i + "에 있다";
+        }   
+    }return answer;
+}
